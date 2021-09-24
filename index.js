8335c1b4faa6ceb1bbc8ce2da7017abbdc35a510
Percy Jonasson <pjonasson@outlook.com>
2021-09-24 12:21:07 -0500

    Added Toggle button for extra info

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = new Vue({
     return {
       message: "These are sports I play!",
       sports: ["football", "baseball", "soccer", "basketball"],
+      extra: false,
     };
   },
   methods: {
